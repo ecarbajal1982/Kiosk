74651ba2ef7c8ecb23fd9d122a9c836cb90220e9
Danny <dau1982@gmail.com>
2014-04-09 10:16:01 -0700

    another

--- include/html_head.php
+++ include/html_head.php
@@ -3,8 +3,8 @@
 <link rel="stylesheet" href="css/font-awesome.min.css">
 
 <!-- Javascript -->
-<script src="js/jquery-2.1.0.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/bootstrap-typeahead.js"></script> 
+<script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-typeahead.js"></script> 
 <script type="text/JavaScript" src="js/sha512.js"></script> 
 <script type="text/JavaScript" src="js/main.js"></script>
